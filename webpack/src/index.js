981d6055b99623d6652af68b3f8dde5f7ffb1a84
Josue <sjosue5082002@gmail.com>
2023-03-28 19:13:59 -0500

    Webpack: 2.3 Proyecto a empaquetar

---
+++ webpack/src/index.js
@@ -0,0 +1,36 @@
+const $links = document.getElementById('links');
+const $name = document.querySelector('h1');
+
+const data = {
+    name: "Josue Muñoz",
+    nickname: "Josue02",
+    description: "...",
+    avatar: "...",
+    social: [
+      {
+        name: "faceebok",
+        url: "https://facebook.com/",
+        username: "",
+      },
+      {
+        name: "instagram",
+        url: "https://instagram.com/",
+        username: "",
+      },
+    ],
+    links: [
+      {
+        name: "blog",
+        url: "https://example.com/",
+        color: "red",
+        emoji: "📖",
+      },
+      {
+        name: "podcast",
+        url: "https://example.com/",
+        color: "yellow",
+        emoji: "💬",
+      },
+    ],
+    footer: "Made with Love on Mexico",
+  };
