8b87834b1683527b9c2b4e458360f384928912ec
Josue <sjosue5082002@gmail.com>
2023-03-30 09:53:18 -0500

    EsBuild: 4.3 Empaquetando con EsBuild

--- esbuild/src/components/App.tsx
+++ esbuild/src/components/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 
 export const App = (p:{m: string}) => {
+    const [c, setC] = useState(0)
+
+    const handleClick = useCallback(() => {
+        setC(c + 1)
+    }, [c])
   return (
     <div>
        
         <h1>{p.m}</h1>
         <h2>Count: {c}</h2>
-        <button >Increment</button>
+        <button onClick={handleClick}>Increment</button>
        
     </div>
   )
