5e3b16a87a4b1cf81a1f312a642639a89a7e638f
Josue <sjosue5082002@gmail.com>
2023-04-16 13:25:04 -0500

    Rollup: 5.2 Configuracion de plugins

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,3 +1,8 @@
+import html from "@rollup/plugin-html"
+import copy from "rollup-plugin-copy"
+import css from "rollup-plugin-css-only"
+import image from "@rollup/plugin-image"
+
 export default {
     input: 'src/index.js',
     output: {
@@ -5,5 +10,14 @@ export default {
         format: 'es',
         sourcemap: true
     },
-    plugins: []
+    plugins: [
+        copy({
+            targets: [
+                { src: 'src/assets/**/*', dest: 'dist/assets' }
+            ]
+        }),
+        css({ output: 'bundle.css' }),
+        html(),
+        image()
+    ]
 }
